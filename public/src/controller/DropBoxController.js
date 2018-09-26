5d9a77746f773e58b51710ab294eaffdefd3bee0
lucaskahl <lucaschangkahl@gmail.com>
2018-09-26 18:26:24 -0300

    Adicionando funcionalidade de renomear

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -45,6 +45,22 @@ class DropBoxController {
 
     initEvents() {
 
+        this.btnRename.addEventListener('click', e=> {
+
+            let li = this.getSelection()[0];
+
+            let file = JSON.parse(li.dataset.file);
+
+            let name = prompt("Digite o nome do arquivo", file.name);
+
+            if(name) {
+
+                file.name = name;
+                this.getFirebaseRef().child(li.dataset.key).set(file);
+            }
+
+        });
+
         this.listFilesEl.addEventListener('selectionchange', e => {
 
             switch(this.getSelection().length) {
@@ -398,6 +414,7 @@ class DropBoxController {
         let li = document.createElement('li');
 
         li.dataset.key = key;
+        li.dataset.file = JSON.stringify(file);
 
         li.innerHTML = `
             ${this.getFileIconView(file)}
